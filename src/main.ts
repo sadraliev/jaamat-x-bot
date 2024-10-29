6e2c82acdc64aad6762dcc6665bbee751e7fb296
sadraliev <a.sadraliev@gmail.com>
2024-10-29 19:19:15 -0400

    feat(server): make an available IPv4

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import './config/environment.config';
 import { webhookCallback } from 'grammy';
 import { fastify } from 'fastify';
 import { bot } from './bot';
-import { envSchema } from './config/environment.config';
 
 const server = fastify();
 
 server.post(`/${bot.token}`, webhookCallback(bot, 'fastify'));
 
-server.listen({ port: Number(process.env.PORT) });
+server.listen(
+  { port: Number(process.env.PORT), host: '0.0.0.0' },
+  function (err, address) {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+    server.log.info(`server listening on ${address}`);
+  }
+);
